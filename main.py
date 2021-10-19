3a58d653c5fda192714ebc3a1d122a56d06c6d8f
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-18 19:53:50 -0700

    Added exception handling

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import pandas
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 dt = {row.letter: row.code for(index, row) in df.iterrows()}
 
-word = input("Please enter a word: ").upper()
-result = [dt[letter] for letter in word]
-print(result)
+loop = True
+while loop:
+    try:
+        word = input("Please enter a word: ").upper()
+        result = [dt[letter] for letter in word]
+    except KeyError:
+        pass
+    else:
+        error = False
+        print(result)
+
